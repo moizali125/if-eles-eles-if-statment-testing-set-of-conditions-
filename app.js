2743248206e4fb7bfeef663da2fa8b72ab2ae777
moizali125 <ha9913083@gmail.com>
2024-04-06 01:04:48 +0500

    assiment

---
+++ app.js
@@ -0,0 +1,68 @@
+// var userncme = prompt(`Please tell your name!`);
+// alert('Hello ' + userncme + '! Welcome to my Website');
+document.write("<br>");
+document.write("<h3>Question no 01</h3>");
+document.write("<br>");
+document.write("<h4>Result</h4>");
+var c = 10;
+document.write(`The value of c is : ` + c);
+document.write("<br>");
+document.write("<h3>.........................................</h3>")
+document.write("<br>");
+document.write('The value of ++c is : ' + ++c);
+document.write("<br>");
+document.write('Now the value of c is ' + c);
+document.write("<br>");
+document.write("<br>");
+document.write('The value of c++ is : ' + c++);
+document.write("<br>");
+document.write('Now the value of c is ' + c);
+document.write("<br>");
+document.write("<br>"); document.write('The vclue of --c is : ' + --c);
+document.write("<br>");
+document.write('Now the value of c is ' + c);
+document.write("<br>");
+document.write("<br>"); document.write('The vclue of c-- is : ' + c--);
+document.write("<br>");
+document.write('Now the value of c is ' + c);
+document.write("<br>");
+document.write("<br>");
+
+document.write("<h3>Question no 02</h3>");
+document.write("<br>");
+var a = 2;
+var b = 1;
+console.log((--a) - (--b) + (++b) + (b--));
+document.write('a is 2');
+document.write("<br>");
+document.write('b is 1');
+document.write("<br>");
+document.write('result is 3');
+document.write("<br>");
+// console.log(--a); is 1
+// console.log(--a - --b); is 0
+// console.log(--a - --b + ++b); is 0
+// console.log(--a - --b + ++b + b--); is -1
+document.write("<br>");
+
+document.write("<h3>Question no 03</h3>");
+document.write("<br>");
+document.write('Try to question example')
+document.write("<br>");
+document.write('<button onclick=" var username = prompt(`Please tell your name!`);alert(`Hello ${username}! Welcome to my Website`);">click her</button>');
+
+document.write("<br>");
+document.write("<br>");
+document.write("<h3>Question no 05</h3>");
+document.write("<br>");
+document.write('Try multiplication table of 5 on lucky number in question example:')
+document.write("<br>");
+document.write('<button onclick=" var usernumber = prompt(`Please put your lacky number!\rMuiltiply by 5`);alert(usernumber*5);">click her</button>');
+document.write("<br>");
+document.write("<br>");
+document.write("<h3>Question no 06</h3>");
+document.write("<br>");
+document.write('<p>This Question for you! &#128077;</p>');
+document.write("<br>");
+document.write("<br>");
+document.write(`<div style="color: blue;background-color: black;"><br /><h1>Best OF luck<span style='font-size:50px;'>&#128075;</span></h1><br /></div>`);
