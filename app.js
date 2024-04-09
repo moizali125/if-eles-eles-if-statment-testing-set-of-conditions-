5201ae9f0eeb393e16d935d220971f8b99fe589f
moizali125 <ha9913083@gmail.com>
2024-04-09 19:37:42 +0500

    New filter atage

--- app.js
+++ app.js
@@ -1,68 +1,150 @@
-// var userncme = prompt(`Please tell your name!`);
-// alert('Hello ' + userncme + '! Welcome to my Website');
 document.write("<br>");
 document.write("<h3>Question no 01</h3>");
 document.write("<br>");
-document.write("<h4>Result</h4>");
-var c = 10;
-document.write(`The value of c is : ` + c);
+document.write("<h4>Result :</h4>");
 document.write("<br>");
-document.write("<h3>.........................................</h3>")
-document.write("<br>");
-document.write('The value of ++c is : ' + ++c);
-document.write("<br>");
-document.write('Now the value of c is ' + c);
+document.write('<button id="btn" onclick=" var input = prompt(`Enter a character:`);var characterType = checkCharacterType(input);alert(characterType);">Click Me</button>');
 document.write("<br>");
 document.write("<br>");
-document.write('The value of c++ is : ' + c++);
-document.write("<br>");
-document.write('Now the value of c is ' + c);
-document.write("<br>");
-document.write("<br>"); document.write('The vclue of --c is : ' + --c);
-document.write("<br>");
-document.write('Now the value of c is ' + c);
-document.write("<br>");
-document.write("<br>"); document.write('The vclue of c-- is : ' + c--);
-document.write("<br>");
-document.write('Now the value of c is ' + c);
-document.write("<br>");
-document.write("<br>");
+function checkCharacterType(character) {
+    // Convert the character to its ASCII code
+    var charCode = character.charCodeAt(0);
+
+    if (charCode >= 48 && charCode <= 57) {
+        return "Number";
+    } else if (charCode >= 65 && charCode <= 90) {
+        return "Uppercase Letter";
+    } else if (charCode >= 97 && charCode <= 122) {
+        return "Lowercase Letter";
+    } else {
+        return "Unknown";
+    }
+}
 
 document.write("<h3>Question no 02</h3>");
 document.write("<br>");
-var a = 2;
-var b = 1;
-console.log((--a) - (--b) + (++b) + (b--));
-document.write('a is 2');
+document.write("<h4>Result :</h4>");
 document.write("<br>");
-document.write('b is 1');
+document.write('<button id="btn" onclick="displayLarger();">Click Me</button>');
+function displayLarger() {
+    // Get the values of the two integers from the user
+    var num1 = parseInt(prompt("Enter the first integer:"));
+    var num2 = parseInt(prompt("Enter the second integer:"));
+
+    // Check if the two integers are equal
+    if (num1 === num2) {
+        alert("The two integers are equal.");
+        // Determine the larger integer
+        var larger = (num1 > num2) ? num1 : num2;
+        alert("The larger integer is: " + larger);
+    }
+}
 document.write("<br>");
-document.write('result is 3');
-document.write("<br>");
-// console.log(--a); is 1
-// console.log(--a - --b); is 0
-// console.log(--a - --b + ++b); is 0
-// console.log(--a - --b + ++b + b--); is -1
 document.write("<br>");
 
 document.write("<h3>Question no 03</h3>");
 document.write("<br>");
-document.write('Try to question example')
+document.write("<h4>Result :</h4>");
 document.write("<br>");
-document.write('<button onclick=" var username = prompt(`Please tell your name!`);alert(`Hello ${username}! Welcome to my Website`);">click her</button>');
+document.write('<button id="btn" onclick="moiza()">Click Me</button>');
+// Prompt the user to enter a number
+function moiza() {
+    var number = prompt("Enter a number:");
+
+    // Check if the number is positive, negative, or zero
+    if (number > 0) {
+        alert("The number is positive.");
+    } else if (number < 0) {
+        alert("The number is negative.");
+    } else {
+        alert("The number is zero.");
+    }
+}
+document.write("<br>");
+document.write("<br>");
+
+//question no 4
+
+document.write("<h3>Question no 04</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques4()">Click Me</button>');
+function ques4() {
+    var text = prompt('please put a vowel word only one...');
+    if (text === 'a' || text === 'e' || text === 'i' || text === 'o' || text === 'u') {
+        alert('This is vowel word! \r Good👋')
+    } else {
+        alert('This is not vowel word!\rVery Bad')
+    }
+};
+document.write("<br>");
+document.write("<br>");
+
+//question no 5
+
+document.write("<h3>Question no 05</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write("<br>");
+document.write('<button id="btn" onclick="ques5()">Click Me</button>');
+function ques5() {
+    var correctPassword = "password123";
+    var userInput = prompt("Enter your password:");
+    if (!userInput) {
+        alert("Please enter your password.");
+    } else if (userInput === correctPassword) {
+        alert("Correct! The password you entered matches the original password.");
+    } else {
+        alert("Incorrect password.");
+        alert('The original password is\rpassword123')
+    }
+}
+document.write("<br>");
+document.write("<br>");
+
+
+//question no 6
+
+document.write("<h3>Question no 06</h3>");
+document.write("<br>");
+document.write("<h4>Result :</h4>");
+document.write('var greeting;</br >    var hour = 13;</br >if (hour < 18) {</br > greeting = "Good day";</br >} else {</br > greeting = "Good evening";</br >} ')
 
 document.write("<br>");
 document.write("<br>");
-document.write("<h3>Question no 05</h3>");
+
+
+//question no 7
+
+document.write("<h3>Question no 07</h3>");
 document.write("<br>");
-document.write('Try multiplication table of 5 on lucky number in question example:')
+document.write("<h4>Result :</h4>");
 document.write("<br>");
-document.write('<button onclick=" var usernumber = prompt(`Please put your lacky number!\rMuiltiply by 5`);alert(usernumber*5);">click her</button>');
+document.write('<button id="btn" onclick="ques7()">Click Me</button>');
+function ques7() {
+    var userInput = prompt("Enter the time in 24-hour clock format (e.g., 1900 for 7pm):");
+
+    var time = parseInt(userInput);
+
+    if (time >= 0 && time < 1200) {
+        console.log("Good morning!");
+        alert("Good morning!");
+    } else if (time >= 1200 && time < 1700) {
+        console.log("Good afternoon!");
+        alert("Good afternoon!");
+    } else if (time >= 1700 && time < 2100) {
+        console.log("Good evening!");
+        alert("Good evening!");
+    } else if (time >= 2100 && time <= 2359) {
+        console.log("Good night!");
+        alert("Good night!");
+    } else {
+        console.log("Invalid input! Please enter a time in 24-hour clock format.");
+        alert("Invalid input! Please enter a time in 24-hour clock format.");
+    }
+
+}
 document.write("<br>");
 document.write("<br>");
-document.write("<h3>Question no 06</h3>");
-document.write("<br>");
-document.write('<p>This Question for you! &#128077;</p>');
-document.write("<br>");
-document.write("<br>");
-document.write(`<div style="color: blue;background-color: black;"><br /><h1>Best OF luck<span style='font-size:50px;'>&#128075;</span></h1><br /></div>`);
+document.write('<div style="color: blue;background-color: black;"><br><h1>Best OF luck<span style="font-size:50px;">👋</span></h1><br></div>')//&#128075
